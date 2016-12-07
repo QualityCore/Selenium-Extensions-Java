499ccb2c152e649bf16038d1587687cf2a5e4d26
Leonardo Carmona <leocarmona_@outlook.com>
2016-12-07 16:44:42 -0200

    Implementado a classe AbstractWrapsElement.

---
+++ src/main/java/com/github/qualitycore/selenium/extensions/elements/AbstractWrapsElement.java
@@ -0,0 +1,42 @@
+package com.github.qualitycore.selenium.extensions.elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsElement;
+
+public abstract class AbstractWrapsElement implements WrapsElement {
+
+	private WebElement wrappedElement;
+
+	public AbstractWrapsElement(WebElement wrappedElement) {
+		if (wrappedElement == null)
+			throw new IllegalArgumentException("The wrapped element can't be null.", new NullPointerException("wrappedElement"));
+
+		this.wrappedElement = wrappedElement;
+	}
+
+	public AbstractWrapsElement(WebDriver webDriver, By locator) {
+		if (webDriver == null)
+			throw new IllegalArgumentException("The web driver can't be null.", new NullPointerException("webDriver"));
+
+		if (locator == null)
+			throw new IllegalArgumentException("The locator can't be null.", new NullPointerException("locator"));
+
+		this.wrappedElement = webDriver.findElement(locator);
+	}
+
+	protected AbstractWrapsElement() {
+
+	}
+
+	@Override
+	public WebElement getWrappedElement() {
+		return wrappedElement;
+	}
+
+	protected void setWrappedElement(WebElement wrappedElement) {
+		this.wrappedElement = wrappedElement;
+	}
+
+}
