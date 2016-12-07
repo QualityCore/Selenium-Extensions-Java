d472e059e1bce5899aa0896d1f498c7fdd3ff2f8
Leonardo Carmona <leocarmona_@outlook.com>
2016-12-07 16:37:28 -0200

    Atualização do contrato do elemento de verificação.

--- src/main/java/com/github/qualitycore/selenium/extensions/elements/contracts/IVerificationElement.java
+++ src/main/java/com/github/qualitycore/selenium/extensions/elements/contracts/IVerificationElement.java
@@ -20,31 +20,17 @@ public interface IVerificationElement extends WrapsElement {
 
 	public boolean textEquals(String expectedText);
 
-	public boolean textNotEquals(String notExpectedText);
-
 	public boolean tagNameEquals(String expectedTagName);
-
-	public boolean tagNameNotEquals(String notExpectedTagName);
 
 	public boolean attributeEquals(String attributeName, String expectedAttributeValue);
 
-	public boolean attributeNotEquals(String attributeName, String notexpectedAttributeValue);
-
 	public boolean cssValueEquals(String propertyName, String expectedCssValue);
-
-	public boolean cssValueNotEquals(String propertyName, String notExpectedCssValue);
 
 	public boolean locationEquals(Point expectedLocation);
 
-	public boolean locationNotEquals(Point notExpectedLocation);
-
 	public boolean sizeEquals(Dimension expectedSize);
 
-	public boolean sizeNotEquals(Dimension notExpectedSize);
-
 	public boolean rectEquals(Rectangle expectedRect);
-
-	public boolean rectNotEquals(Rectangle notExpectedRect);
 
 	public WebElement getWrappedElement();
 	
