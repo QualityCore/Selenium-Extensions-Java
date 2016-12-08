c2cea3ea43aea5c9243a040231ed13bb77a6099c
Leonardo Carmona <leocarmona_@outlook.com>
2016-12-08 16:11:10 -0200

    Implementado o contrato do componente web.

---
+++ src/main/java/com/github/qualitycore/selenium/extensions/elements/contracts/IWebComponent.java
@@ -0,0 +1,28 @@
+package com.github.qualitycore.selenium.extensions.elements.contracts;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsElement;
+
+public interface IWebComponent extends WebElement, IWrapsValidationElement, IWrapsVerificationElement, WrapsElement {
+
+	public boolean isPresent();
+
+	public boolean isClicable();
+
+	public void moveToElement();
+
+	public void moveToElement(int xOffset, int yOffset);
+
+	public void clickAndHold();
+
+	public void release();
+
+	public void doubleClick();
+
+	public void contextClick();
+
+	public void dragAndDrop(WebElement target);
+
+	public void dragAndDropBy();
+
+}
